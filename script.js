da6013fda0e4b3b4e31028110e7423d293ca66d0
TristanGomez32 <rossington.jimmy@gmail.com>
2025-07-23 19:35:55 +0200

    Added background image

--- script.js
+++ script.js
@@ -16,37 +16,46 @@ const audioTracks = [
   {
     id: "again_victory",
     label: "Orchestral victory",
-    src: "https://media.githubusercontent.com/media/TristanGomez32/Portfolio/refs/heads/main/demos/again victory.mp3"
+    src: "https://media.githubusercontent.com/media/TristanGomez32/Portfolio/refs/heads/main/demos/again victory.mp3",
+    bckgr_image: "./img/crops/enceinte_crop1.png"
   },
   {
     id: "hardtech",
     label: "Hardtech",
-    src: "./demos/hardtech.mp3"
+    src: "./demos/hardtech.mp3",
+    bckgr_image: "./img/crops/skid_crop1.png"
   },
   {
     id: "sax",
     label: "Akward saxophone",
-    src: "./demos/summer tape sax.mp3"
+    src: "./demos/summer tape sax.mp3",
+    bckgr_image: "./img/crops/lelit_mo_crop1.png",
   },
   {
     id: "romewillburn",
     label: "Hard rock",
-    src: "./demos/rome will burn.mp3"
+    src: "./demos/rome will burn.mp3",
+    bckgr_image: "./img/crops/affiche-skid-768x0-c-default_blurred_crop1.png",
+    background_size: "250px",
   },
   {
     id: "lelitcue2",
     label: "Ambient synths",
-    src: "./demos/lelit cue 2.mp3"
+    src: "./demos/lelit cue 2.mp3",
+    bckgr_image: "./img/crops/hjalmar-wahlin-08_blurred_crop1.png",
   },
   {
     id: "whatyoudidfolk",
     label: "Folk guitar",
-    src: "./demos/what you did folk.mp3"
+    src: "./demos/what you did folk.mp3",
+    bckgr_image: "./img/crops/John_Singer_Sargent_-_Gabriel_Fauré_crop1_blur.png",
+    background_size: "220px",
   },
   {
     id: "trailerlush",
     label: "Lush strings",
-    src: "./demos/trailer_lush.mp3"
+    src: "./demos/trailer lush.mp3",
+    bckgr_image: "./img/crops/enceinte_crop2.png"
   }  
 ];
 
@@ -81,16 +90,31 @@ function onClick (demo_audio_id,ctrl_id) {
 
 const container = document.getElementById("demos");
 
-  audioTracks.forEach(({ id, label, src }) => {
+  audioTracks.forEach((info) => {
+
+    console.log(info);
+
+    var id = info.id;
+    var label = info.label;
+    var src = info.src;
+    var bckgr_image = info.bckgr_image;
+
     const li = document.createElement("li");
     li.className = "demo";
+    
+    li.style.backgroundImage = "url('"+bckgr_image+"')";
+    
+    if (info.hasOwnProperty("background_size")){
+      li.style.backgroundSize = info.background_size;
+    }
+    
     li.innerHTML = `
       <audio class="demo_audio" id="${id}_audio" preload="none">
         <source src="${src}" type="audio/mpeg" />
       </audio>
       <text class="demo_label">${label}</text>
       <button class="demo_audio_btn" id="${id}_btn" onclick="onClick('${id}_audio','${id}_btn')">▶</button>
-    `;
+      `;
     container.appendChild(li);
   });
 
