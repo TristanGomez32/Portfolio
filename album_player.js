527680e05413dd0d4f654cb8957b5fc904f2e7e3
TristanGomez32 <rossington.jimmy@gmail.com>
2025-07-29 17:29:47 +0200

    Added progress bar

--- album_player.js
+++ album_player.js
@@ -1,18 +1,66 @@
+function make_play_symbol(color){
+  return `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="24" cy="24" r="22" fill="white" />
+            <path d="M20 16L32 24L20 32V16Z" fill="${color}"/>
+            </svg> 
+          `
+}
+
+function make_pause_symbol(color){
+  return `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <!-- Cercle de fond -->
+            <circle cx="24" cy="24" r="22" fill="white"></circle>
+            <!-- Barres du "pause" -->
+            <path d="M19 16H22V32H19V16Z" fill="${color}"></path>
+            <path d="M26 16H29V32H26V16Z" fill="${color}"></path>
+          </svg>
+          `
+}
 
 const audio = document.getElementById("audioPlayer");
 const playBtn = document.getElementById("mainPlayBtn");
+const seekBar = document.getElementById("seekbar_romewillburn");
+const seekBar_container = document.getElementById("seekbar_container_romewillburn");
+
 const tracks = document.querySelectorAll(".track-list li");
+
+BTN_COLOR= "#2c2c2c"
 
 function playTrack(track) {
     const src = track.getAttribute("data-src");
-    audio.src = src;
+    
+    if (audio.src != src){
+      audio.src = src;
+      duration_str_split = track.querySelectorAll(".track-duration")[0].innerHTML.split(":");
+      audio.dataset.duration = 60*parseInt(duration_str_split[0])+parseInt(duration_str_split[1]);
+      console.log("DURATION",duration_str_split,60*parseInt(duration_str_split[0])+parseInt(duration_str_split[1]),audio.dataset);
+    }
     audio.play();
-    playBtn.textContent = "⏸";
-    // Highlight track
-    tracks.forEach((t) => t.classList.remove("active"));
-    track.classList.add("active");
+    console.log("2ND try",audio.duration);
+    playBtn.innerHTML = make_pause_symbol(BTN_COLOR);
+    tracks.forEach((t) => t.classList.remove("active-track"));
+    tracks.forEach((t) => t.querySelectorAll(".track-number")[0].classList.remove("active-track"));
+    tracks.forEach((t) => t.querySelectorAll(".track-number")[0].dataset.playing = false);
 
+    track.classList.add("active-track");
+    track.querySelectorAll(".track-number")[0].classList.add("active-track");
+    track.querySelectorAll(".track-number")[0].dataset.playing = true;
 }
+
+
+playBtn.addEventListener('click', () => {
+
+  for (track of tracks){
+    if (track.getAttribute("data-src") == audio.src){
+      break
+    }
+  }
+
+  track.click();
+
+});
+
+
 
 tracks.forEach(track => {
   track.addEventListener('click', () => {
@@ -21,14 +69,36 @@ tracks.forEach(track => {
     tracks.forEach(t => t.dataset.playing = "false");
 
     track.dataset.playing = isPlaying ? "false" : "true";
+    track.querySelectorAll(".track-number")[0].playing = track.dataset.playing;
 
     if (isPlaying){
         audio.pause();
-        playBtn.textContent = "▶";
+        playBtn.innerHTML = make_play_symbol(BTN_COLOR);
     } else {
         playTrack(track);
-        playBtn.textContent = "⏸";
+        playBtn.innerHTML = make_pause_symbol(BTN_COLOR);
     }
 
   });
 });
+
+
+play_btn_list = document.querySelectorAll(".play-btn")
+
+play_btn_list.forEach((btn) => {
+  btn.innerHTML = make_play_symbol(BTN_COLOR);
+});
+
+
+  audio.addEventListener('timeupdate', () => {
+    const progressPercent = (audio.currentTime / audio.dataset.duration) * 100;
+    seekBar.style.width = `${progressPercent}%`;
+  });
+
+
+  seekBar_container.addEventListener('click', (e) => {
+    const containerWidth = seekBar_container.clientWidth;
+    const offsetX = e.offsetX;
+    const newTime = (offsetX / containerWidth) * audio.dataset.duration;
+    audio.currentTime = newTime;
+  });
