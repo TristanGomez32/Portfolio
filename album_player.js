5581374297edfc7f6703fbfb69f6d6a3f233769e
TristanGomez32 <rossington.jimmy@gmail.com>
2025-08-03 22:53:55 +0200

    Added short song name

--- album_player.js
+++ album_player.js
@@ -1,5 +1,109 @@
 BTN_COLOR= "#2c2c2c"
 
+/* GENERATE ALBUM */
+
+function generateAlbumHTML(folderPath, albumName, genre, tracks, genres, track_durations) {
+  const coverUrl = `${folderPath}cover.jpg`;
+
+  const trackItemsHTML = tracks.map((track, index) => {
+    const fileName = track.split('/').pop();
+    const baseName = fileName.replace('.mp3', '').replace(/_/g, ' ');
+    const title = baseName
+      .split(' ')
+      .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+      .join(' ');
+    const trackUrl = `${folderPath}${track}`;
+
+    return `
+      <li class="track" data-src="${trackUrl}" data-playing="false">
+        <span class="track-number">${index + 1}</span>
+        <div class="track-info">
+          <span class="track-title">${title}</span>
+          <span class="track-artist">${genres[index]}</span>
+        </div>
+        <span class="track-duration">${track_durations[index]}</span>
+      </li>`;
+  }).join('\n');
+
+  return `
+    <div class="album_section">
+      <div class="album_subsection">
+        <img src="${coverUrl}" alt="Album cover" class="big-album-cover" style="object-fit: cover;">
+      </div>
+      <div class="album_subsection">
+        <div class="album-player">
+          <div class="album-header">
+            <div class="album-header-top">
+              <img src="${coverUrl}" alt="Album cover" class="album-cover">
+              <div class="album-info">
+                <h2>${albumName}</h2>
+                <p>${genre}</p>
+              </div>
+              <button class="play-btn"></button>
+            </div>
+            <div class="seekbar-container">
+              <div class="seekbar"></div>
+              <div class="seekbar-handle"></div>
+            </div>
+          </div>
+          <div class="album-body">
+            <ul class="track-list">
+              ${trackItemsHTML}
+            </ul>
+          </div>
+          <audio class="album-audio"></audio>
+        </div>
+      </div>
+    </div>`;
+}
+
+metadatas = [{"folder":"https://media.githubusercontent.com/media/TristanGomez32/Portfolio/refs/heads/main/albums/rome_will_burn/",
+                  "albumName": "Rome will burn",
+                  "genre": "Hard rock / Folk / Orchestral",
+                  "tracks_to_add":["rome_will_burn.mp3","again.mp3","what_you_did.mp3"],
+                  "genres":["Hard rock","Orchestral","Folk / Orchestral"],
+                  "track_durations":["02:31","02:55","03:11"]
+            },
+            {"folder":"https://media.githubusercontent.com/media/TristanGomez32/Portfolio/refs/heads/main/albums/weird_signal/",
+                  "albumName": "Weird Signal",
+                  "genre": "Electronic",
+                  "tracks_to_add":["intruder_on_board.mp3","weird_signal.mp3"],
+                  "genres":["Electronic trailer","Synthwave"],
+                  "track_durations":["01:27","01:02"]
+            },
+            {"folder":"https://media.githubusercontent.com/media/TristanGomez32/Portfolio/refs/heads/main/albums/classical_work/",
+                  "albumName": "Classical work",
+                  "genre": "Baroque / Classical / Post-romantic",
+                  "tracks_to_add":["fugue_for_organ.mp3","song_for_violin_and_piano.mp3","theme_and_variations.mp3"],
+                  "genres":["Baroque","Classical","Post-romantic"],
+                  "track_durations":["03:30","06:38","01:53"]
+            },
+]
+
+var albumHTML;
+
+for (metadata of metadatas){
+  albumHTML = generateAlbumHTML(metadata["folder"], metadata["albumName"], metadata["genre"], metadata["tracks_to_add"], metadata["genres"],metadata["track_durations"]);
+  document.querySelectorAll(".albums")[0].innerHTML += albumHTML;
+
+}
+
+
+
+/* GENERATE ALBUM END */
+
+
+
+
+
+
+
+
+
+
+
+
+
 function make_play_symbol(color){
   return `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="24" cy="24" r="22" fill="white" />
@@ -20,6 +124,7 @@ function make_pause_symbol(color){
 }
 
 function playTrack(track) {
+    console.log("playtrack",track);
     const src = track.getAttribute("data-src");
     
     album = track.parentElement.parentElement.parentElement;
@@ -124,3 +229,5 @@ for (album of albums){
   });
 
 }
+
+
