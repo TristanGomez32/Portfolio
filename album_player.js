68fad6bb296c874a73b4818f3125119f1970e230
TristanGomez32 <rossington.jimmy@gmail.com>
2025-08-06 00:06:35 +0200

    Fixed responsiveness issue

--- album_player.js
+++ album_player.js
@@ -151,10 +151,10 @@ for (metadata of metadatas["albums"]){
 }
 
 albumHTML = generateAlbumHTML(metadatas["short_movies"]["lelit"],show_big_cover=false); 
-document.getElementById("lelit_player").innerHTML += albumHTML;
+document.querySelectorAll(".bg-lit")[0].innerHTML += albumHTML;
 
 albumHTML = generateAlbumHTML(metadatas["short_movies"]["summer_tape"],show_big_cover=false); 
-document.getElementById("summer_tape_player").innerHTML += albumHTML;
+document.querySelectorAll(".bg-sunbed")[0].innerHTML += albumHTML;
 
 /* GENERATE ALBUM END */
 
