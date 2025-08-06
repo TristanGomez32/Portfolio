ffeb12aac677ad1c8c0ee37458cd80e55eaed866
TristanGomez32 <rossington.jimmy@gmail.com>
2025-08-06 21:21:25 +0200

    improved contextual menu

--- menu.js
+++ menu.js
@@ -1,20 +1,24 @@
 
-const page_sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.side-menu a');
 
-const section_observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    const id = entry.target.getAttribute('id');
-    const link = document.querySelector(`.side-menu a[href="#${id}"]`);
 
-    if (entry.isIntersecting) {
-      navLinks.forEach(el => el.classList.remove('active'));
-      if (link) link.classList.add('active');
-    }
+menuPage = document.querySelectorAll(".menu-page")[0];
+
+
+menuBtn = document.querySelectorAll(".menu-btn")[0];
+menuBtn.addEventListener('click',(e) => {menuPage.dataset.isshown=true});
+
+btns = document.querySelectorAll(".menu-page a");
+
+for (btn of btns){
+
+  btn.addEventListener("click",(e) => {
+    menuPage.dataset.isshown = false;
   });
-}, {
-  threshold: 0.1  // le seuil d'intersection : 50% de la section visible
+
+}
+
+
+closeBtn = document.getElementById("quit-menu-btn");
+closeBtn.addEventListener("click",(e)=>{
+  menuPage.dataset.isshown = false;
 });
-
-page_sections.forEach(section => section_observer.observe(section));
-
