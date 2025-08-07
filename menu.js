dc1758c38388f26f52b888fca646289787c16af9
TristanGomez32 <rossington.jimmy@gmail.com>
2025-08-07 16:49:52 +0200

    Hides scroll btn text on small screen

--- menu.js
+++ menu.js
@@ -22,3 +22,35 @@ closeBtn = document.getElementById("quit-menu-btn");
 closeBtn.addEventListener("click",(e)=>{
   menuPage.dataset.isshown = false;
 });
+
+
+
+// Make menu bttn appear only after reaching short movies section 
+  document.addEventListener("DOMContentLoaded", () => {
+  sectionHeadings = document.querySelectorAll(".section-heading");
+
+  for (section of sectionHeadings){
+    if(section.id == "shortmovies"){
+      break
+    }
+  }
+
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (!entry.isIntersecting) {
+        // Le titre est collé en haut → afficher le bouton
+        menuBtn.style.display = "block";
+      } else {
+        // Le titre n'est pas encore en haut → cacher le bouton
+        menuBtn.style.display = "none";
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: "-1px 0px 0px 0px", // On détecte quand le titre est sticky
+    }
+  );
+
+  observer.observe(section);
+});
